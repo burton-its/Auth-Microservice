9f171e5f3a66b43c5a53c4fdd24e728ae98db5cb
Sarah Van Hoose <vanhooss@oregonstate.edu>
2026-03-08 13:08:18 -0500

    validator and test program

--- TestProgram/private/anotherpage.js
+++ TestProgram/private/anotherpage.js
@@ -1,5 +1,28 @@
 const myButton = document.querySelector('button');
 
 myButton.addEventListener('click', async () => {
-    alert('STILL WORKS')
+    const tokenResponse = await fetch('http://localhost:9000/get-token',{
+    mode: 'cors',
+    method: 'POST',
+    credentials: 'include',
+})
+    const data = await tokenResponse.json();
+    const my_token = data.token;
+
+    const tokenRevoke = await fetch('http://localhost:9000/revoke', {
+        mode: 'cors',
+        method: 'POST',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ token: my_token })
+    });
+
+    if (tokenRevoke.ok) {
+        await fetch('http://localhost:9000/cleanup',{
+        mode: 'cors',
+        method: 'DELETE',
+        credentials: 'include',
+        })
+    }
+    window.location.href = 'http://localhost:4000/';
 });
